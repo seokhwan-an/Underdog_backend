965ef190429af2641eb7c742fece8068bfb88095
이건회 <rg970604@naver.com>
2022-08-09 13:23:52 +0900

    init

---
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/UnderdogCountyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.underdogCounty.underdogCountyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderdogCountyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderdogCountyProjectApplication.class, args);
+	}
+
+}
