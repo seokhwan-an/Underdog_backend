ed8940a98177b45838e0a1aeff420808fb0386f7
이건회 <rg970604@naver.com>
2022-08-22 18:12:56 +0900

    fix: fix builder pattern application creation request

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/application/request/ApplicationCreationRequest.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/application/request/ApplicationCreationRequest.java
@@ -1,11 +1,36 @@
 package com.underdogCounty.underdogCountyProject.domain.application.request;
 
+import com.underdogCounty.underdogCountyProject.domain.application.entity.Application;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@ToString
+@NoArgsConstructor
 public class ApplicationCreationRequest {
+    private Long id;
     private String name;
     private String email;
     private String phonenumber;
     private String contents;
+
+    public Application toEntity(){
+        Application build = Application.builder()
+                .id(id)
+                .name(name)
+                .email(email)
+                .phonenumber(phonenumber)
+                .build();
+        return build;
+    }
+    @Builder
+    public ApplicationCreationRequest(Long id, String name, String email, String phonenumber, String contents){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.phonenumber=phonenumber;
+        this.contents=contents;
+    }
 }
