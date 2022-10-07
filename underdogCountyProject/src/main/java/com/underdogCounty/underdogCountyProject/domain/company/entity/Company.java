cc69be29d7b4ca8028bf49dc435738eac2e5c823
SeokHwan An <asok369258@naver.com>
2022-10-07 15:02:59 +0900

    feature #28 : 파일 구조 변경으로 인한 수정

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/company/entity/Company.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/company/entity/Company.java
@@ -1,8 +1,11 @@
-package com.underdogCounty.underdogCountyProject.domain.company;
+package com.underdogCounty.underdogCountyProject.domain.company.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.underdogCounty.underdogCountyProject.domain.work.Work;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,8 +13,10 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
-@Setter
 @Entity
 @Table(name = "company")
 public class Company {
