d12bd995447a98ed69ab6cbdb6d0ba3d43adc048
SeokHwan An <asok369258@naver.com>
2022-10-11 23:13:14 +0900

    feature #26 : delete image

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/image/Type.java
+++
@@ -1,5 +0,0 @@
-package com.underdogCounty.underdogCountyProject.domain.image;
-
-public enum Type {
-    회사, 아티스트, 작업물
-}
--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/image/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.underdogCounty.underdogCountyProject.domain.image.repository;
-
-import com.underdogCounty.underdogCountyProject.domain.image.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
