99770d26257543463f17c5dd6f08952cebae323a
이건회 <rg970604@naver.com>
2022-12-13 04:30:33 +0900

    refactor ApplicationRepository.java

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/application/repository/ApplicationRepository.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/application/repository/ApplicationRepository.java
@@ -1,7 +1,9 @@
 package com.underdogCounty.underdogCountyProject.domain.application.repository;
 
 import com.underdogCounty.underdogCountyProject.domain.application.entity.Application;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationRepository extends JpaRepository<Application,Long> {
+    List<Application> findAllByName(String name);
 }
