a47e4c992033de0fe2200d6dfbeeda5f2d0fe852
이건회 <rg970604@naver.com>
2022-12-24 06:10:24 +0900

    feat AboutRepository.java

---
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/about/repository/AboutRepository.java
@@ -0,0 +1,8 @@
+package com.underdogCounty.underdogCountyProject.domain.about.repository;
+
+import com.underdogCounty.underdogCountyProject.domain.about.entity.About;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AboutRepository extends JpaRepository<About, Long> {
+
+}
