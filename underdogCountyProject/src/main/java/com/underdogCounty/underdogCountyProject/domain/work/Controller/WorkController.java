d9fdd410c6e7a4daec08f51c9ab5a290c6e0b7ca
seokhwan-an <asok369258@naver.com>
2023-01-11 19:57:06 +0900

    feat #33 : 작업물 전체 가져오기 기능 추가

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/work/Controller/WorkController.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/work/Controller/WorkController.java
@@ -2,16 +2,19 @@ package com.underdogCounty.underdogCountyProject.domain.work.Controller;
 
 import com.underdogCounty.underdogCountyProject.domain.work.Work;
 import com.underdogCounty.underdogCountyProject.domain.work.dto.WorkRequestDto;
+import com.underdogCounty.underdogCountyProject.domain.work.dto.WorkResponseDto;
 import com.underdogCounty.underdogCountyProject.domain.work.service.WorkService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("api/work")
-@Controller
+@RestController
 public class WorkController {
 
     private final WorkService workService;
@@ -21,4 +24,8 @@ public class WorkController {
         return workService.create(workRequestDto);
     }
 
+    @GetMapping()
+    public List<WorkResponseDto> getAll() {
+        return workService.getAll();
+    }
 }
--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/work/service/WorkService.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/work/service/WorkService.java
@@ -2,7 +2,10 @@ package com.underdogCounty.underdogCountyProject.domain.work.service;
 
 import com.underdogCounty.underdogCountyProject.domain.work.Work;
 import com.underdogCounty.underdogCountyProject.domain.work.dto.WorkRequestDto;
+import com.underdogCounty.underdogCountyProject.domain.work.dto.WorkResponseDto;
 import com.underdogCounty.underdogCountyProject.domain.work.repository.WorkRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +21,13 @@ public class WorkService {
         work.requestToEntity(workRequestDto);
         workRepository.save(work);
         return work;
+    }
+
+    public List<WorkResponseDto> getAll() {
+        List<Work> works = workRepository.findAll();
+        List<WorkResponseDto> result = works.stream()
+            .map(s -> new WorkResponseDto().entityToResponse(s))
+            .collect(Collectors.toList());
+        return result;
     }
 }
