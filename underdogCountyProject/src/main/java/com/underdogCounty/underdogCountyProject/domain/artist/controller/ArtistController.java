f595818417248328138cd2ecf8a34b05f2a28341
seokhwan-an <asok369258@naver.com>
2023-01-11 17:18:52 +0900

    feat #42 : 아티스트 프로필 생성 controller 추가

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/artist/controller/ArtistController.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/artist/controller/ArtistController.java
@@ -2,10 +2,13 @@ package com.underdogCounty.underdogCountyProject.domain.artist.controller;
 
 import com.underdogCounty.underdogCountyProject.domain.artist.dto.ArtistRequestDto;
 import com.underdogCounty.underdogCountyProject.domain.artist.dto.ArtistResponseDto;
+import com.underdogCounty.underdogCountyProject.domain.artist.entity.Artist;
 import com.underdogCounty.underdogCountyProject.domain.artist.service.ArtistService;
 import com.underdogCounty.underdogCountyProject.domain.util.ResponseEntity;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,38 +16,46 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RequestMapping("api/artist")
 @RestController
 public class ArtistController {
 
-  private final ArtistService artistService;
+    private final ArtistService artistService;
 
-  @PostMapping()
-  public ResponseEntity<ArtistRequestDto> create(@RequestBody ArtistRequestDto artistRequestDto) {
-    return artistService.create(artistRequestDto);
-  }
+    @PostMapping()
+    public ResponseEntity<ArtistRequestDto> create(@RequestBody ArtistRequestDto artistRequestDto) {
+        return artistService.create(artistRequestDto);
+    }
 
-  @GetMapping()
-  public ResponseEntity<List<ArtistResponseDto>> getAll() {
-    return artistService.getAll();
-  }
+    @GetMapping()
+    public ResponseEntity<List<ArtistResponseDto>> getAll() {
+        return artistService.getAll();
+    }
 
-  @GetMapping("{id}")
-  public ResponseEntity<ArtistResponseDto> getOne(@PathVariable Long id) {
-    return artistService.getOne(id);
-  }
+    @GetMapping("{id}")
+    public ResponseEntity<ArtistResponseDto> getOne(@PathVariable Long id) {
+        return artistService.getOne(id);
+    }
 
-  @PutMapping("{id}")
-  public ResponseEntity<ArtistRequestDto> update(@PathVariable Long id,
-      @RequestBody ArtistRequestDto artistRequestDto) {
-    return artistService.update(id, artistRequestDto);
-  }
+    @PutMapping("{id}")
+    public ResponseEntity<ArtistRequestDto> update(@PathVariable Long id,
+        @RequestBody ArtistRequestDto artistRequestDto) {
+        return artistService.update(id, artistRequestDto);
+    }
 
-  @DeleteMapping("{id}")
-  public ResponseEntity<Void> delete(@PathVariable Long id){
-    return artistService.delete(id);
-  }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return artistService.delete(id);
+    }
+
+    @PostMapping(value = "s3/{artistId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Artist saveS3Image(@PathVariable Long artistId,
+        @RequestParam(value = "profile") MultipartFile image) throws IOException {
+        return artistService.uploadS3Image(artistId, image);
+    }
 }
