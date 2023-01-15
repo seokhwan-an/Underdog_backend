adea8a27a76544c72cd371efef288a3d70c768d7
이건회 <rg970604@naver.com>
2023-01-15 20:10:04 +0900

    refactor ExceptionHandlerController.java

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/exception/handler/ExceptionHandlerController.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/exception/handler/ExceptionHandlerController.java
@@ -2,8 +2,14 @@ package com.underdogCounty.underdogCountyProject.domain.exception.handler;
 
 import com.underdogCounty.underdogCountyProject.domain.exception.ExceptionCode;
 import com.underdogCounty.underdogCountyProject.domain.exception.WrongArtist;
+import com.underdogCounty.underdogCountyProject.domain.exception.WrongEmail;
+import com.underdogCounty.underdogCountyProject.domain.exception.WrongPhoneNumber;
 import com.underdogCounty.underdogCountyProject.domain.exception.WrongWork;
+import javax.validation.ConstraintViolationException;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +27,24 @@ public class ExceptionHandlerController {
     @ExceptionHandler(WrongWork.class)
     public ErrorResult wrongWorkId(IllegalArgumentException e) {
         return new ErrorResult(ExceptionCode.WRONG_WORK);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongEmail.class)
+    public ErrorResult wrongEmailPattern(IllegalArgumentException e) {
+        return new ErrorResult(ExceptionCode.WRONG_EMAIL);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongPhoneNumber.class)
+    public ErrorResult wrongPhoneNumberPattern(IllegalArgumentException e) {
+        return new ErrorResult(ExceptionCode.WRONG_PHONENUMBER);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResult nullInput(MethodArgumentNotValidException e) {
+        return new ErrorResult(ExceptionCode.NULL_INPUT);
     }
 
 }
