d0e14ad2133d02d4bbbf4f33e25d131fc006dd2c
이건회 <rg970604@naver.com>
2023-01-15 16:45:51 +0900

    refactor WorkResponseDto.java

--- underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/work/dto/WorkResponseDto.java
+++ underdogCountyProject/src/main/java/com/underdogCounty/underdogCountyProject/domain/work/dto/WorkResponseDto.java
@@ -6,13 +6,14 @@ import lombok.Data;
 
 @Data
 public class WorkResponseDto {
-
+    private Long id;
     private String image;
 
     private Category category;
 
     public WorkResponseDto entityToResponse(Work work) {
         WorkResponseDto result = new WorkResponseDto();
+        result.id = work.getId();
         result.image = work.getImage();
         result.category = work.getCategory();
         return result;
